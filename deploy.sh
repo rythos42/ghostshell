c559b70c6b25e69495c6458ac367467a431f13ef
Craig Fleming <rythos42@gmail.com>
2018-12-09 14:14:48 -0800

    First attempt at an autodeploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "------------------------------"
+echo "Uploading client"
+for entry in src/client/build/*
+do
+  if [ -f "$entry" ] && [ "$entry" != "config.json" ] then
+    echo "Uploading $entry"
+    curl --ftp-create-dirs -T $entry -u $FTP_USER:$FTP_PASS ftp://geeksong.com/public_html/ghostshell/$entry
+  fi
+done
+
+echo "------------------------------"
+echo "Uploading server"
+for entry in src/server/*
+do
+  if [ -f "$entry" ] && [ "$entry" != "config.php" ] && [ "$entry" != "vendor" ] && [ "$entry" != "composer.json" ]&& [ "$entry" != "composer.lock" ] then
+    echo "Uploading $entry"
+    curl --ftp-create-dirs -T $entry -u $FTP_USER:$FTP_PASS ftp://geeksong.com/public_html/ghostshell/srv/$entry
+  fi
+done
