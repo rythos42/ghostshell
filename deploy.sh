aac4d39079befec7e024b27674fc5b4a2c4dc5be
Craig Fleming <rythos42@gmail.com>
2018-12-10 21:31:45 -0800

    Fixing url parameter order, changing Travis variable spacing, praying

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,10 @@ for entry in src/client/build/*
 do
   if [ -f "$entry" ] && [ "$entry" != "config.json" ]; then
     echo "Uploading $entry"
-    curl -T $entry -u ftp://geeksong.com/public_html/ghostshell/ $FTP_USER:$FTP_PASS 
+    curl -T $entry ftp://geeksong.com/public_html/ghostshell/ -u $FTP_USER:$FTP_PASS 
   fi
 done
-for entry in src/client/build/static*
+for entry in src/client/build/static/*
 do
   if [ -f "$entry" ]; then
     echo "Uploading $entry"
