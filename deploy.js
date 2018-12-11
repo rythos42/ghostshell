4e0fbb8194828690d38086958115eb1bb59fff63
Craig Fleming <rythos42@gmail.com>
2018-12-11 07:19:58 -0800

    Fixed deployment js locally, it works, now to see if it works on Travis

--- deploy.js
+++ deploy.js
@@ -6,21 +6,31 @@ var config = {
   password: process.env.FTP_PASSWORD,
   host: "ftp.geeksong.com",
   port: 21,
-  localRoot: __dirname + "/src",
+  localRoot: __dirname + "/src/client/build",
   remoteRoot: "/public_html/ghostshell/",
-  include: ["client/build/*", "server/*"],
-  exclude: [
-    "config.json",
-    "vendor",
-    "config.php",
-    "composer.json",
-    "composer.lock"
-  ],
-  deleteRemote: true,
+  include: ["**/*"],
+  exclude: ["config.json"],
   forcePasv: true
 };
 
+console.log("Deploying client");
 ftpDeploy
   .deploy(config)
-  .then(res => console.log("finished"))
+  .then(res => {
+    console.log("Finished client");
+    console.log("Deploying server");
+    config.localRoot = __dirname + "/src/server";
+    config.remoteRoot = "/public_html/ghostshell/srv";
+    config.exclude = [
+      "vendor/**/*.*",
+      "config.php",
+      "composer.json",
+      "composer.lock"
+    ];
+
+    ftpDeploy
+      .deploy(config)
+      .then(res => console.log("Finished server"))
+      .catch(err => console.log(err));
+  })
   .catch(err => console.log(err));
