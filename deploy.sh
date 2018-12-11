9d8359d549ba300ef3892ec04d19664f1212479c
Craig Fleming <rythos42@gmail.com>
2018-12-10 21:21:53 -0800

    Adding static folder to deploy, move args around, praying it works.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,14 @@ for entry in src/client/build/*
 do
   if [ -f "$entry" ] && [ "$entry" != "config.json" ]; then
     echo "Uploading $entry"
-    curl --ftp-create-dirs -T $entry -u $FTP_USER:$FTP_PASS ftp://geeksong.com/public_html/ghostshell/$entry
+    curl -T $entry -u ftp://geeksong.com/public_html/ghostshell/ $FTP_USER:$FTP_PASS 
+  fi
+done
+for entry in src/client/build/static*
+do
+  if [ -f "$entry" ]; then
+    echo "Uploading $entry"
+    curl -T $entry ftp://geeksong.com/public_html/ghostshell/static/ -u $FTP_USER:$FTP_PASS 
   fi
 done
 
@@ -16,6 +23,6 @@ for entry in src/server/*
 do
   if [ -f "$entry" ] && [ "$entry" != "config.php" ] && [ "$entry" != "vendor" ] && [ "$entry" != "composer.json" ] && [ "$entry" != "composer.lock" ]; then
     echo "Uploading $entry"
-    curl --ftp-create-dirs -T $entry -u $FTP_USER:$FTP_PASS ftp://geeksong.com/public_html/ghostshell/srv/$entry
+    curl -T $entry ftp://geeksong.com/public_html/ghostshell/srv/ -u $FTP_USER:$FTP_PASS 
   fi
 done
