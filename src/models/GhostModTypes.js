fa285757a4405ada139cb6a1291686cd0f48fcfd
Craig Fleming <rythos42@gmail.com>
2018-12-29 08:49:35 -0800

    Doing a regex to match ghost mod types

--- src/models/GhostModTypes.js
+++ src/models/GhostModTypes.js
@@ -52,46 +52,45 @@ function getByHash(array, hash) {
   return array[hash] || array[hash - 4294967296];
 }
 
-const ghostModTypeMappings = {
-  ' caches': 'Caches',
-  ' resources': 'Resources',
-  ' xp': 'XP',
-  ' experience': 'XP',
-  ' glimmer': 'Glimmer',
-  ' faction consumables': 'FactionConsumables',
-  ' telemetry': 'Telemetry',
-  ' bright engram': 'BrightEngram',
-  exotic: 'Exotic', // this one happens at the beginning of the sentence
-  ' io': 'Io',
-  ' hellas basin': 'HellasBasin',
-  ' mercury': 'Mercury',
-  ' tangled shore': 'TangledShore',
-  ' dreaming city': 'DreamingCity',
-  ' titan': 'Titan',
-  ' edz': 'EDZ',
-  ' nessus': 'Nessus',
-  ' leviathan': 'Leviathan',
-  ' gambit': 'Gambit',
-  ' crucible': 'Crucible',
-  ' strikes': 'Strikes',
-  ' public events': 'PublicEvents',
-  ' solar weapon': 'SolarWeapon',
-  ' arc weapon': 'ArcWeapon',
-  ' Void Weapon': 'VoidWeapon',
-  ' elemental weapon': 'ElementalWeapon',
-  ' generated': 'Generated',
-  ' vehicle less time to summon': 'VehicleLessTimeToSummon',
-  ' reload your weapon': 'ReloadYourWeapon',
-  ' ride': 'Ride'
-};
+const ghostModTypeMappings = [
+  { regex: /\bcaches\b/gi, ghostModType: 'Caches' },
+  { regex: /\bresources\b/gi, ghostModType: 'Resources' },
+  { regex: /\bxp\b/gi, ghostModType: 'XP' },
+  { regex: /\bexperience\b/gi, ghostModType: 'XP' },
+  { regex: /\bglimmer\b/gi, ghostModType: 'Glimmer' },
+  { regex: /\bfaction consumables\b/gi, ghostModType: 'FactionConsumables' },
+  { regex: /\btelemetry\b/gi, ghostModType: 'Telemetry' },
+  { regex: /\bbright engram\b/gi, ghostModType: 'BrightEngram' },
+  { regex: /\bexotic\b/gi, ghostModType: 'Exotic' },
+  { regex: /\bio\b/gi, ghostModType: 'Io' },
+  { regex: /\bhellas basin\b/gi, ghostModType: 'HellasBasin' },
+  { regex: /\bmercury\b/gi, ghostModType: 'Mercury' },
+  { regex: /\btangled shore\b/gi, ghostModType: 'TangledShore' },
+  { regex: /\bdreaming city\b/gi, ghostModType: 'DreamingCity' },
+  { regex: /\btitan\b/gi, ghostModType: 'Titan' },
+  { regex: /\bedz\b/gi, ghostModType: 'EDZ' },
+  { regex: /\bnessus\b/gi, ghostModType: 'Nessus' },
+  { regex: /\bleviathan\b/gi, ghostModType: 'Leviathan' },
+  { regex: /\bgambit\b/gi, ghostModType: 'Gambit' },
+  { regex: /\bcrucible\b/gi, ghostModType: 'Crucible' },
+  { regex: /\bstrikes\b/gi, ghostModType: 'Strikes' },
+  { regex: /\bpublic events\b/gi, ghostModType: 'PublicEvents' },
+  { regex: /\bsolar weapon\b/gi, ghostModType: 'SolarWeapon' },
+  { regex: /\barc weapon\b/gi, ghostModType: 'ArcWeapon' },
+  { regex: /\bvoid weapon\b/gi, ghostModType: 'VoidWeapon' },
+  { regex: /\belemental weapon\b/gi, ghostModType: 'ElementalWeapon' },
+  { regex: /\bgenerated\b/gi, ghostModType: 'Generated' },
+  { regex: /\bvehicle less time to summon\b/gi, ghostModType: 'VehicleLessTimeToSummon' },
+  { regex: /\breload your weapon\b/gi, ghostModType: 'ReloadYourWeapon' },
+  { regex: /\bride\b/gi, ghostModType: 'Ride' }
+];
 
 function getGhostModTypesForString(description) {
-  const lowerDesc = description.toLowerCase();
-  return Object.keys(ghostModTypeMappings)
-    .filter(ghostModTypeMappingString => {
-      return lowerDesc.indexOf(ghostModTypeMappingString.toLowerCase()) !== -1;
+  return ghostModTypeMappings
+    .filter(ghostModTypeMapping => {
+      return ghostModTypeMapping.regex.test(description);
     })
-    .map(ghostModTypeMappingString => {
-      return ghostModTypeMappings[ghostModTypeMappingString];
+    .map(ghostModTypeMapping => {
+      return ghostModTypeMapping.ghostModType;
     });
 }
